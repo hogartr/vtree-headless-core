93a524a902efc4fd1cb240f636f0626d4908cae8
hogartr <hogartr@gmail.com>
2025-08-01 00:42:15 +0200

    fixed unnecessary rerenders in vtree-render

--- src/vtree-render/src/components/VariableSizeTree.tsx
+++ src/vtree-render/src/components/VariableSizeTree.tsx
@@ -1,5 +1,5 @@
 import { useVirtualizer, type Virtualizer } from '@tanstack/react-virtual';
-import React, { useCallback, useEffect, useMemo, useRef, type ComponentType } from 'react';
+import React, { useCallback, useMemo, useRef, type ComponentType } from 'react';
 
 import type { FlatTree, Node } from '../types';
 
@@ -24,65 +24,68 @@ interface VariableSizeTreeProps {
   testId?: string;
 }
 
-export const VariableSizeTree = React.memo(function VariableSizeTreeComponent({
-  children: Row,
-  flatTree,
-  width,
-  height,
-  itemSize,
-  ref,
-  overscan = 0,
-  testId,
-}: VariableSizeTreeProps) {
-  const parentRef = useRef<HTMLDivElement>(null);
+// eslint-disable-next-line react/display-name
+export const VariableSizeTree = React.memo(
+  ({
+    children: Row,
+    flatTree,
+    width,
+    height,
+    itemSize,
+    ref,
+    overscan = 0,
+    testId,
+  }: VariableSizeTreeProps) => {
+    const parentRef = useRef<HTMLDivElement>(null);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const getItemSize = useCallback((node: Node) => itemSize(node), []);
-  const getItemKey = useCallback((index: number) => flatTree.data[index].id, [flatTree.data]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const getItemSize = useCallback((node: Node) => itemSize(node), []);
+    const getItemKey = useCallback((index: number) => flatTree.data[index].id, [flatTree.data]);
 
-  const rowVirtualizer = useVirtualizer({
-    count: flatTree.data.length,
-    getScrollElement: () => parentRef.current,
-    estimateSize: index => getItemSize(flatTree.data[index]),
-    getItemKey,
-    overscan,
-  });
+    const rowVirtualizer = useVirtualizer({
+      count: flatTree.data.length,
+      getScrollElement: () => parentRef.current,
+      estimateSize: index => getItemSize(flatTree.data[index]),
+      getItemKey,
+      overscan,
+    });
 
-  if (ref) {
-    ref.current = rowVirtualizer;
-  }
+    if (ref) {
+      ref.current = rowVirtualizer;
+    }
 
-  return (
-    <div
-      ref={parentRef}
-      style={{
-        width,
-        height,
-        overflow: 'auto',
-      }}
-      data-testid={testId}
-    >
+    return (
       <div
+        ref={parentRef}
         style={{
-          height: `${rowVirtualizer.getTotalSize()}px`,
-          width: '100%',
-          position: 'relative',
+          width,
+          height,
+          overflow: 'auto',
         }}
+        data-testid={testId}
       >
-        {rowVirtualizer.getVirtualItems().map(({ index, start, key }) => (
-          <RenderRow
-            index={index}
-            start={start}
-            flatTree={flatTree}
-            Row={Row}
-            getItemSize={getItemSize}
-            key={key}
-          />
-        ))}
+        <div
+          style={{
+            height: `${rowVirtualizer.getTotalSize()}px`,
+            width: '100%',
+            position: 'relative',
+          }}
+        >
+          {rowVirtualizer.getVirtualItems().map(({ index, start, key }) => (
+            <RenderRow
+              index={index}
+              start={start}
+              flatTree={flatTree}
+              Row={Row}
+              getItemSize={getItemSize}
+              key={key}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 interface RenderRowProps {
   Row: RowComponent;
@@ -92,13 +95,8 @@ interface RenderRowProps {
   getItemSize: ItemSize;
 }
 
-const RenderRow = React.memo(function RenderRowComponent({
-  index,
-  start,
-  flatTree,
-  Row,
-  getItemSize,
-}: RenderRowProps) {
+// eslint-disable-next-line react/display-name
+const RenderRow = React.memo(({ index, start, flatTree, Row, getItemSize }: RenderRowProps) => {
   const height = getItemSize(flatTree.data[index]);
   const style: React.CSSProperties = useMemo(
     () => ({
